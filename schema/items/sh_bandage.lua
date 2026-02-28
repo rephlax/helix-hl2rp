1978b7ec333a88aa3d9481e8ead9fef8fa12d780
Luke Farrell <lukefarrell2012@gmail.com>
2026-02-28 01:44:41 +0000

    Bandage overheal addition

--- schema/items/sh_bandage.lua
+++ schema/items/sh_bandage.lua
@@ -1,15 +1,22 @@
-
 ITEM.name = "Bandage"
 ITEM.model = Model("models/props_wasteland/prison_toiletchunk01f.mdl")
 ITEM.description = "A small roll of hand-made gauze."
 ITEM.category = "Medical"
-ITEM.price = 18
+
+-- CONFIG
+
+local HEAL_AMOUNT = 20
+local OVERHEAL_BONUS = 10
 
 ITEM.functions.Apply = {
-	sound = "items/medshot4.wav",
-	OnRun = function(itemTable)
-		local client = itemTable.player
+    sound = "items/medshot4.wav",
+    OnRun = function(itemTable)
+        local client = itemTable.player
+        local currentHP = client:Health()
+        local maxHP = client:GetMaxHealth()
 
-		client:SetHealth(math.min(client:Health() + 20, 100))
-	end
+        client:SetHealth(math.min(currentHP + HEAL_AMOUNT, maxHP + OVERHEAL_BONUS))
+
+        return true
+    end
 }
